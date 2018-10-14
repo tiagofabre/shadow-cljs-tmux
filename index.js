3538db7bb50bccf51dbb3201b537dae23753ef94
Tiago Fabre <tiagofabre@gmail.com>
2018-10-14 12:56:39 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+const pparams = process.argv.slice(2).join(' ')
+const currentPath = process.cwd();
+const spawn = require('child_process').spawn;
+const options = {cwd: currentPath, shell: process.env.SHELL};
+const shadow = require('child_process').spawn(`/usr/local/bin/shadow-cljs ${pparams}`, options);
+
+const gray = '#eff0f1'
+
+function setBgColor (color) {
+  spawn('tmux', ['set-option', 'status-left-bg', color])
+  spawn('tmux', ['set-option', 'status-left-fg', 'black'])
+}
+
+setBgColor(gray)
+
+function selectColor(data) {
+  if(data.includes('Compiling')) {
+    setBgColor("yellow")
+  } else if (data.includes('Build completed')) {
+    setBgColor("green")
+  } else if (data.includes('Build failure')) {
+    setBgColor("red")
+  } else if (data.includes('running')) {
+    setBgColor(gray)
+  }
+}
+
+shadow.stdout.on('data', (data) => {
+  selectColor(data)
+  console.log(`${data}`)
+})
+
+shadow.stderr.on('data', (data) => {
+  selectColor(data)
+  console.log(`${data}`)
+})
+
+shadow.on('close', (code) => {
+  console.log(`child process exited with code ${code}`)
+})
+
+function exitHandler(options, exitCode) {
+  setBgColor('default')
+  shadow.kill('SIGINT')
+  process.exit()
+}
+
+process.on('exit', exitHandler.bind(null,{cleanup:true}))
+process.on('SIGINT', exitHandler.bind(null, {exit:true}))
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}))
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}))
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}))
---
+++ readme.md
@@ -0,0 +1,13 @@
+A simple package to update the tmux status line based in shadow-cljs builds
+
+#Installation
+
+```
+npm install shadow-cljs-tmux -g
+```
+
+#Usage
+
+```
+shadow-cljs-tmux watch <<app>>
+```
